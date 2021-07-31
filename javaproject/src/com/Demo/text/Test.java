4f942a39337f32dc6979e468e7ef362a4cf73438
TL <TL@gmail.com>
2021-07-31 11:08:10 +0530

    created Test.java by TL

---
+++ javaproject/src/com/Demo/text/Test.java
@@ -0,0 +1,13 @@
+package com.Demo.text;
+
+import com.Demo.beans.utility;
+
+public class Test {
+	
+	public static void main(string[] args) {
+		utility u = new utility();
+		System.out.println(u);
+	}
+	
+
+}
