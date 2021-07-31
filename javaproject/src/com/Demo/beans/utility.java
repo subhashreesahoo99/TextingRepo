c5b06ea3953d0a5fbd7579303796c2f2e706ccc2
TL <TL@gmail.com>
2021-07-31 09:49:05 +0530

    first commit

---
+++ javaproject/src/com/Demo/beans/utility.java
@@ -0,0 +1,12 @@
+package com.Demo.beans;
+
+import java.util.Date;
+
+public class utility {
+	
+	public Date getsystemDate()
+	{
+		return new Date();
+	}
+
+}
---
+++ javaproject/src/javaproject/Class.java
@@ -0,0 +1,11 @@
+package javaproject;
+
+public class Class {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
